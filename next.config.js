d956717f3cfa09ff0ab89f229bc7f00fa6909ef9
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-12-03 21:18:19 +0100

    fix: package json

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@
 // };
 
 // module.exports = nextConfig;
+// const path = require("path");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.resolve.alias["leaflet"] = path.resolve(
+//       __dirname,
+//       "node_modules/leaflet"
+//     );
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
@@ -16,7 +30,11 @@ const nextConfig = {
     );
     return config;
   },
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin']
+  }
 };
 
 module.exports = nextConfig;
+
 
